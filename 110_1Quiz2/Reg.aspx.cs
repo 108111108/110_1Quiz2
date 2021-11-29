59ef6dd0292b8c3f5baa607a19d2e2c609b5b4fa
孟陞 彭 <108111108@mail.aeust.edu.tw>
2021-11-29 19:52:57 +0800

    Quiz 2

--- 110_1Quiz2/Reg.aspx.cs
+++ 110_1Quiz2/Reg.aspx.cs
@@ -6,8 +6,15 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace _110_1Quiz2 {
-    public partial class Reg : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+    public partial class Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            lb_Msg.Text = tb_CTEL.Text;
 
         }
     }
